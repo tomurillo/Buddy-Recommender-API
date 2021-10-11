9dc40febd347f10199713cccea9e2048195fcedc
tomurillo <tomurillo@gmail.com>
2021-10-11 15:36:04 +0200

    Add user model and start auth work

---
+++ src/buddy_recommender/main/model/user.py
@@ -0,0 +1,79 @@
+import datetime
+import jwt
+
+from typing import Union
+
+from .. import db, flask_bcrypt
+from ..config import key as app_secret_key
+from sqlalchemy.sql import func
+
+
+class Account(db.Model):
+    """
+    Account Model for storing user accounts that have access to the API
+    """
+    __tablename__ = "account"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    created = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    admin = db.Column(db.Boolean, nullable=False, default=False)
+    password_hash = db.Column(db.String(100))
+
+    @property
+    def password(self):
+        raise AttributeError('Password is write-only')
+
+    @password.setter
+    def password(self, password: str):
+        """
+        Safely store the user' password
+        :param password: password in plain text
+        :return: None; password is hashed and stored
+        """
+        self.password_hash = flask_bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password: str) -> bool:
+        """
+        Check whether the given password matches the user's password
+        :param password: password to check against the user's
+        :return: True if both passwords match; False otherwise
+        """
+        return flask_bcrypt.check_password_hash(self.password_hash, password)
+
+    @staticmethod
+    def encode_auth_token(account_id: int) -> bytes:
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1, seconds=5),
+            'iat': datetime.datetime.utcnow(),
+            'sub': account_id
+        }
+        return jwt.encode(
+            payload,
+            app_secret_key,
+            algorithm='HS256'
+        )
+
+    @staticmethod
+    def decode_auth_token(auth_token: str) -> Union[str, int]:
+        """
+        Decodes the auth token
+        :param auth_token:
+        :return: integer|string
+        """
+        try:
+            return jwt.decode(auth_token, app_secret_key)
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
+
+    def __repr__(self):
+        return f"<uid {self.id}>"
+
+    def __str__(self):
+        return f"User <id={self.id}; email={self.email}; admin={self.admin}>"
---
+++ src/buddy_recommender/main/util/auth_helper.py
@@ -0,0 +1,84 @@
+from buddy_recommender.main.model.user import Account
+from typing import Dict, Tuple
+
+
+class Auth:
+
+    @staticmethod
+    def login_user(data: Dict[str, str]) -> Tuple[Dict[str, str], int]:
+        try:
+            user = Account.query.filter_by(email=data.get('email')).first()
+            if user and user.check_password(data.get('password')):
+                auth_token = Account.encode_auth_token(user.id)
+                if auth_token:
+                    response_object = {
+                        'status': 'success',
+                        'message': 'Successfully logged in.',
+                        'Authorization': auth_token.decode()
+                    }
+                    return response_object, 200
+            else:
+                response_object = {
+                    'status': 'fail',
+                    'message': 'email or password does not match.'
+                }
+                return response_object, 401
+
+        except Exception as e:
+            print(e)
+            response_object = {
+                'status': 'fail',
+                'message': 'Try again'
+            }
+            return response_object, 500
+
+    @staticmethod
+    def logout_user(data: str) -> Tuple[Dict[str, str], int]:
+        auth_token = data.split(" ")[1] if data else ''
+        if auth_token:
+            resp = Account.decode_auth_token(auth_token)
+            if not isinstance(resp, str):
+                # mark the token as blacklisted
+                return save_token(token=auth_token)
+            else:
+                response_object = {
+                    'status': 'fail',
+                    'message': resp
+                }
+                return response_object, 401
+        else:
+            response_object = {
+                'status': 'fail',
+                'message': 'Provide a valid auth token.'
+            }
+            return response_object, 403
+
+    @staticmethod
+    def get_logged_in_user(new_request):
+        # get the auth token
+        auth_token = new_request.headers.get('Authorization')
+        if auth_token:
+            resp = Account.decode_auth_token(auth_token)
+            if not isinstance(resp, str):
+                user = Account.query.filter_by(id=resp).first()
+                response_object = {
+                    'status': 'success',
+                    'data': {
+                        'user_id': user.id,
+                        'email': user.email,
+                        'admin': user.admin,
+                        'registered_on': str(user.registered_on)
+                    }
+                }
+                return response_object, 200
+            response_object = {
+                'status': 'fail',
+                'message': resp
+            }
+            return response_object, 401
+        else:
+            response_object = {
+                'status': 'fail',
+                'message': 'Provide a valid auth token.'
+            }
+            return response_object, 401
