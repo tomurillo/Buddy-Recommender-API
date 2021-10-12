8f65cda269ecd9fcd8168117d3352c516049232a
tomurillo <tomurillo@gmail.com>
2021-10-12 12:25:53 +0200

    Add model for blacklisted tokens

---
+++ src/buddy_recommender/main/model/blacklisttoken.py
@@ -0,0 +1,29 @@
+from .. import db
+from sqlalchemy.sql import func
+
+
+class BlacklistToken(db.Model):
+    """
+    Token Model for storing JWT tokens
+    """
+    __tablename__ = 'blacklist_tokens'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    token = db.Column(db.String(500), unique=True, nullable=False)
+    blacklisted_on = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
+    def __init__(self, token):
+        self.token = token
+
+    def __repr__(self):
+        return '<id: token: {}'.format(self.token)
+
+    @staticmethod
+    def check_blacklist(auth_token: str) -> bool:
+        """
+        Check whether the given auth token has been blacklisted
+        :param auth_token: an auth token string
+        :return: True if token has been blacklisted; False otherwise
+        """
+        res = BlacklistToken.query.filter_by(token=str(auth_token)).first()
+        return bool(res)
