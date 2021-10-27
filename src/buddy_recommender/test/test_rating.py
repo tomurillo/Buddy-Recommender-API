5bf69b71a6aaebdd55b2dae5aff8ea5b8dc2d804
tomurillo <tomurillo@gmail.com>
2021-10-27 10:42:38 +0200

    Fix rating tests

--- src/buddy_recommender/test/test_rating.py
+++ src/buddy_recommender/test/test_rating.py
@@ -7,6 +7,7 @@ API_VERSIONS = ['v1']
 
 
 def add_rating(self, user_id, item_id, rating, auth_token, api_version):
+    headers = dict(Authorization=f'Bearer {auth_token}') if auth_token else None
     return self.client.post(
         f'/api/{api_version}/rating/',
         data=json.dumps({
@@ -14,7 +15,7 @@ def add_rating(self, user_id, item_id, rating, auth_token, api_version):
             'item_id': item_id,
             'rating': rating,
         }),
-        headers=dict(Authorization=f'Bearer {auth_token}'),
+        headers=headers,
         content_type='application/json'
     )
 
@@ -27,33 +28,32 @@ class TestRatingBlueprint(BaseTestCase):
         with self.client:
             login_data = create_account_and_validate(self)
             auth_token = login_data['Authorization']
-            for api in API_VERSIONS:
-                # Add new rating
-                response = add_rating(self, 1, 2, 5, auth_token, api)
-                response_data = json.loads(response.data.decode())
-                self.assertEqual(response.status_code, 201)
-                self.assertTrue(response_data['status'] == 'success')
-                self.assertTrue(response_data['message'] == 'New rating added.')
-                # Retrieve added rating
-                response = self.client.get(f'/api/{api}/rating/user/1/item/2',
-                                           headers=dict(Authorization=f'Bearer {auth_token}'))
-                response_data = json.loads(response.data.decode())
-                self.assertEqual(response.status_code, 200)
-                self.assertEqual(response_data['data']['user_id'], 1)
-                self.assertEqual(response_data['data']['item_id'], 2)
-                self.assertEqual(response_data['data']['rating'], 5)
-                self.assertTrue(response_data['data']['created'])
-                self.assertTrue(response_data['data']['updated'])
-                # Same request, change parameter order
-                response = self.client.get(f'/api/{api}/rating/item/2/user/1',
-                                           headers=dict(Authorization=f'Bearer {auth_token}'))
-                response_data = json.loads(response.data.decode())
-                self.assertEqual(response.status_code, 200)
-                self.assertEqual(response_data['data']['item_id'], 2)
-                self.assertEqual(response_data['data']['user_id'], 1)
-                self.assertEqual(response_data['data']['rating'], 5)
-                self.assertTrue(response_data['data']['created'])
-                self.assertTrue(response_data['data']['updated'])
+            # Add new rating
+            response = add_rating(self, 1, 2, 5, auth_token, API_VERSIONS[0])
+            response_data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertTrue(response_data['status'] == 'success')
+            self.assertTrue(response_data['message'] == 'New rating added.')
+            # Retrieve added rating
+            response = self.client.get(f'/api/{API_VERSIONS[0]}/rating/user/1/item/2',
+                                       headers=dict(Authorization=f'Bearer {auth_token}'))
+            response_data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response_data['data']['user_id'], 1)
+            self.assertEqual(response_data['data']['item_id'], 2)
+            self.assertEqual(response_data['data']['rating'], 5)
+            self.assertTrue(response_data['data']['created'])
+            self.assertTrue(response_data['data']['updated'])
+            # Same request, change parameter order
+            response = self.client.get(f'/api/{API_VERSIONS[0]}/rating/item/2/user/1',
+                                       headers=dict(Authorization=f'Bearer {auth_token}'))
+            response_data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response_data['data']['item_id'], 2)
+            self.assertEqual(response_data['data']['user_id'], 1)
+            self.assertEqual(response_data['data']['rating'], 5)
+            self.assertTrue(response_data['data']['created'])
+            self.assertTrue(response_data['data']['updated'])
 
     def test_missing_rating(self):
         """
@@ -62,17 +62,16 @@ class TestRatingBlueprint(BaseTestCase):
         with self.client:
             login_data = create_account_and_validate(self)
             auth_token = login_data['Authorization']
-            for api in API_VERSIONS:
-                # Add new rating
-                response = add_rating(self, 1, 2, 5, auth_token, api)
-                self.assertEqual(response.status_code, 201)
-                # Request rating not in database
-                response = self.client.get(f'/api/{api}/rating/user/1/item/3',
-                                           headers=dict(Authorization=f'Bearer {auth_token}'))
-                response_data = json.loads(response.data.decode())
-                self.assertEqual(response.status_code, 404)
-                self.assertTrue(response_data['status'] == 'fail')
-                self.assertTrue(response_data['message'][:32] == 'No rating for user 1 and item 3.')
+            # Add new rating
+            response = add_rating(self, 1, 2, 5, auth_token, API_VERSIONS[0])
+            self.assertEqual(response.status_code, 201)
+            # Request rating not in database
+            response = self.client.get(f'/api/{API_VERSIONS[0]}/rating/user/1/item/3',
+                                       headers=dict(Authorization=f'Bearer {auth_token}'))
+            response_data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertTrue(response_data['status'] == 'fail')
+            self.assertTrue(response_data['message'][:32] == 'No rating for user 1 and item 3.')
 
     def test_rating_no_auth(self):
         """
@@ -81,16 +80,15 @@ class TestRatingBlueprint(BaseTestCase):
         fake_auth_token = 'asdf123'
         with self.client:
             create_account_and_validate(self)
-            for api in API_VERSIONS:
-                # No token
-                response = add_rating(self, 1, 2, 5, None, api)
-                response_data = json.loads(response.data.decode())
-                self.assertEqual(response.status_code, 401)
-                self.assertTrue(response_data['status'] == 'fail')
-                self.assertTrue(response_data['message'] == 'Invalid token. Please log in again.')
-                # Invalid token
-                response = add_rating(self, 1, 2, 5, fake_auth_token, api)
-                response_data = json.loads(response.data.decode())
-                self.assertEqual(response.status_code, 401)
-                self.assertTrue(response_data['status'] == 'fail')
-                self.assertTrue(response_data['message'] == 'Invalid token. Please log in again.')
+            # No token
+            response = add_rating(self, 1, 2, 5, None, API_VERSIONS[0])
+            response_data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 401)
+            self.assertTrue(response_data['status'] == 'fail')
+            self.assertTrue(response_data['message'] == 'Provide a valid auth token.')
+            # Invalid token
+            response = add_rating(self, 1, 2, 5, fake_auth_token, API_VERSIONS[0])
+            response_data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 401)
+            self.assertTrue(response_data['status'] == 'fail')
+            self.assertTrue(response_data['message'] == 'Invalid token. Please log in again.')
